2aa252c0637d8b8f435392d0b1216997daa3c423
yanlei <0049003135@znv.com>
2019-04-01 19:34:12 +0800

    select directory

--- Simulator/src/main/java/sam/znv/view/OutPutPanel.java
+++ Simulator/src/main/java/sam/znv/view/OutPutPanel.java
@@ -5,11 +5,9 @@ import sam.znv.controller.SendController;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 /**
  * 数据配置信息
@@ -58,11 +56,42 @@ public class OutPutPanel extends JPanel {
         sendBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-                SendController.sendIdx1(filepath,1,1);
+                //SendController.sendIdx1(filepath,1,1);
                 System.out.println("发送：" + filepath);
+                sendPic(filepath);
 			}
 		});
+
+        JLabel labelFeature = new JLabel("输入特征：");
+        labelFeature.setBounds(40, 60, 100, 30);
+        this.add(labelFeature);
+
+        JTextField featureInput = new JTextField();
+        featureInput.setBounds(100, 60, 160, 30);
+        this.add(featureInput);
+
+        JButton featureSendBtn = new JButton("发送特征");
+        featureSendBtn.setBounds(280, 60, 100, 30);
+        this.add(featureSendBtn);
+        featureSendBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SendController.sendIdx2(featureInput.getText());
+            }
+        });
+
     }
 
-
+    private void sendPic(String path)
+    {
+        File f = new File(path);
+        if (f.isDirectory()) {
+            String s[] = f.list();
+            for (int i = 0; i < s.length; i++) {
+                sendPic(path + "\\" + s[i]);
+            }
+        } else {
+            SendController.sendIdx1(path,1,1);
+        }
+    }
 }
